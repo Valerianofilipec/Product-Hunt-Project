6b3d79849cf2e331a19380609ab298afc79d704a
Valeriano Filipe Calológio <valeriofilipec@gmail.com>
2020-05-22 03:58:30 +0100

    Requisição de Produtos da API

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './components/Header'
 import './styles.css'
-
+import Main from './pages/main'
 
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
     </div>
   );
 }
