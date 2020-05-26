c82ac4a862c31e9d8a7cc77e85cfbbdcdd67b3ab
Valeriano Filipe Calológio <valeriofilipec@gmail.com>
2020-05-26 01:59:17 +0100

    restart with mobile

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 //Iniciando a App
 const app = express()
 app.use(express.json()) //permitir o envio de informações(as requisições) no formato json
-app.use(cors())// permitindo acesso acesso externo 
+app.use(cors())// permitindo acesso externo 
 
 //Iniciando a DB
 mongoose.connect('mongodb://localhost:27017/backend', {
